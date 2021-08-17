b5551ad8b815c03dfb23f95961c2b3e753f6fb74
sanghunlee <simoniful0gmail.com>
2021-08-17 12:09:44 +0900

    Fix: resolve merge logic conflict

--- src/App.js
+++ src/App.js
@@ -1,71 +1,71 @@
-
-import React, { useState } from "react";
-import styled from "styled-components";
-import Form from "./components/Form";
-import Result from "./components/Result";
-import Timer from "./components/Timer";
-import useForm from "./hooks/useForm";
-import { validate } from "./utils/regex";
-
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Form from './components/Form';
+import Result from './components/Result';
+import Timer from './components/Timer';
+import useForm from './hooks/useForm';
 
 export default function App() {
-	const [sortAscending, setSortAscending] = useState("");
-	const [sortDescending, setSortDescending] = useState("");
-	const dateOpt = { dateStyle: "full" };
-	const korDate = new Date().toLocaleDateString("ko-KR", dateOpt);
-	const engDate = new Date().toLocaleDateString("en-US", dateOpt);
+  const [sortAscending, setSortAscending] = useState('');
+  const [sortDescending, setSortDescending] = useState('');
+  const dateOpt = { dateStyle: 'full' };
+  const korDate = new Date().toLocaleDateString('ko-KR', dateOpt);
+  const engDate = new Date().toLocaleDateString('en-US', dateOpt);
 
+  const handleSort = (currentedValue) => {
+    console.log(currentedValue);
+    const newSortAscending = customSort(correctedValue, 'asc');
+    const newSortDescending = customSort(correctedValue, 'desc');
+    setSortAscending(newSortAscending);
+    setSortDescending(newSortDescending);
+    return true;
+  };
 
-	const customSorting = (arr, opt) => {
-		// string -> array
-		// sort 메서드 구현 -> 내림/오름 차순
-		for (let i = 0; i < arr.length; i++) {
-			let swap;
-			for (let j = 0; j < arr.length - 1; j++) {
-				if (arr[j] > arr[j + 1]) {
-					swap = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = swap;
-				}
-			}
-			if (!swap) {
-				break;
-			}
-		}
-		if (opt === "desc") {
-			return arr.toString();
-		} else {
-			return arr.reverse().toString();
-		}
-		// setting State
-		// concat
-	};
+  const customSort = (arr, opt) => {
+    // string -> array
+    // sort 메서드 구현 -> 내림/오름 차순
+    for (let i = 0; i < arr.length; i++) {
+      let swap;
+      for (let j = 0; j < arr.length - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          swap = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = swap;
+        }
+      }
+      if (!swap) {
+        break;
+      }
+    }
+    if (opt === 'desc') {
+      return arr.toString();
+    } else {
+      return arr.reverse().toString();
+    }
+    // setting State
+    // concat
+  };
 
-	const { values, errors, handleChange, handleSubmit } = useForm(
-		customSorting,
-		validate
-	);
+  const { value, correctedValue, handleChange, handleSubmit } =
+    useForm(handleSort);
 
-	return (
-		<Wrap>
-			<Timer>{korDate}</Timer>
-			<Form
-				values={values}
-				errors={errors}
-				handleSubmit={handleSubmit}
-				handleChange={handleChange}
-			></Form>
-			<Result>{customSorting([1, 3, 2, 5], "desc")}</Result>
-			<Result>{customSorting([1, 3, 2, 5], "de")}</Result>
-			<Timer>{engDate}</Timer>
-		</Wrap>
-	);
-
+  return (
+    <Wrap>
+      <Timer>{korDate}</Timer>
+      <Form
+        value={value}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}></Form>
+      <Result>{sortAscending}</Result>
+      <Result>{sortDescending}</Result>
+      <Timer>{engDate}</Timer>
+    </Wrap>
+  );
 }
 
 const Wrap = styled.div`
-	margin: 30px auto;
-	padding: 5px 10px;
-	width: 500px;
-	background-color: lightgray;
+  margin: 30px auto;
+  padding: 5px 10px;
+  width: 500px;
+  background-color: lightgray;
 `;
