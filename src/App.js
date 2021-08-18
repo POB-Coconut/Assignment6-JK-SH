c0b7d733eb51cba737d6c763f7a2247c37c4aa54
jaekyung lee <jaekyunglee@jaekyungui-MacBookPro.local>
2021-08-18 10:29:42 +0900

    Fix: deployTest

--- src/App.js
+++ src/App.js
@@ -1,49 +1,50 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import Form from './components/Form';
-import Result from './components/Result';
-import Timer from './components/Timer';
-import useForm from './hooks/useForm';
-import { customSort } from './utils/customSort';
-import { mergeSort } from './utils/mergeSort';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Form from "./components/Form";
+import Result from "./components/Result";
+import Timer from "./components/Timer";
+import useForm from "./hooks/useForm";
+import { customSort } from "./utils/customSort";
 
 export default function App() {
-  const [sortAscending, setSortAscending] = useState('');
-  const [sortDescending, setSortDescending] = useState('');
+	const [sortAscending, setSortAscending] = useState("");
+	const [sortDescending, setSortDescending] = useState("");
+	console.log("deploy Success");
 
-  const handleSort = (correctedValue) => {
-    const newSortAscending = customSort(correctedValue, 'asc').toString();
-    const newSortDescending = customSort(correctedValue, 'desc').toString();
-    new Promise((resolve, _reject) => {
-      setSortAscending(newSortAscending);
-      setTimeout(() => {
-        resolve();
-      }, 3000);
-    }).then(() => {
-      setSortDescending(newSortDescending);
-    });
-    return true;
-  };
+	const handleSort = (correctedValue) => {
+		const newSortAscending = customSort(correctedValue, "asc").toString();
+		const newSortDescending = customSort(correctedValue, "desc").toString();
+		new Promise((resolve, _reject) => {
+			setSortAscending(newSortAscending);
+			setTimeout(() => {
+				resolve();
+			}, 3000);
+		}).then(() => {
+			setSortDescending(newSortDescending);
+		});
+		return true;
+	};
 
-  const { value, handleChange, handleSubmit } = useForm(handleSort);
+	const { value, handleChange, handleSubmit } = useForm(handleSort);
 
-  return (
-    <Wrap>
-      <Timer localeType="ko-KR" />
-      <Form
-        value={value}
-        handleSubmit={handleSubmit}
-        handleChange={handleChange}></Form>
-      <Result>{sortAscending}</Result>
-      <Result>{sortDescending}</Result>
-      <Timer localeType="en-US" />
-    </Wrap>
-  );
+	return (
+		<Wrap>
+			<Timer localeType="ko-KR" />
+			<Form
+				value={value}
+				handleSubmit={handleSubmit}
+				handleChange={handleChange}
+			></Form>
+			<Result>{sortAscending}</Result>
+			<Result>{sortDescending}</Result>
+			<Timer localeType="en-US" />
+		</Wrap>
+	);
 }
 
 const Wrap = styled.div`
-  margin: 30px auto;
-  padding: 5px 10px;
-  width: 500px;
-  background-color: lightgray;
+	margin: 30px auto;
+	padding: 5px 10px;
+	width: 500px;
+	background-color: lightgray;
 `;
